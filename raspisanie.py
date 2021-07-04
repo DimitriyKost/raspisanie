f5eb97d1f7d9995b6282444830b035f1dd513c67
DimitriyKost <dimkos08@gmail.com>
2021-07-04 15:52:51 +0300

    рабочий вариант, но надо еще посмотреть как добавить спаны

--- raspisanie.py
+++ raspisanie.py
@@ -8,37 +8,24 @@ file_htm = '01.htm'
 
 with open (file_htm,'r') as f:
     html = BeautifulSoup(f.read(), 'html.parser')
-filteredNews = []
-allNews = []
+#filteredNews = []
+#allNews = []
 #print(html)
 pars1 = html.findAll('tr')
-
-#print (pars1[0])
-##for data in pars1:
-##    if data.find('td') is not None:
-##        filteredNews.append(data.text)
-txtH="   <td style='text-align: center;'><span style='font-size: 14pt; color: #ff0000;'>"
-print (txtH)
+txtH="   <td style='text-align: center;'><span style='font-size: 14pt;'>"
+#print (txtH)
 txtE="</td>\n"
 txtR=""
-for data in pars1:
-#    if data.find('span') is not None:
-        pars2 = data.findAll('td')
-
-#        print(pars2[1])
-        for txt in pars2:
-            pars3 = txt.find('span').text
+for i in range (len(pars1)):
+        pars2 = pars1[i].findAll('td')
+        txtR+="<tr>\n"
+        for j in range (len(pars2)):
+            pars3 = pars2[j].find('span').text
+            pars3=pars3.replace('\n ',' ')
+            pars3=pars3.replace('  ',' ')
             txtR+=txtH+pars3+txtE
-            # price = text.replace(' ', '').replace('₽', '')
-            #print(txtH+pars3+txtE)
-            tx=filteredNews.append(pars3)
-            #print(txtH+tx+txtE)
-            #txtR+=txtH+tx+txtE
-            #print(tx)
-
-ttx=txtR.replace('\n ',' ')
-#надо убрать пробелы и разделить по дням
-print(ttx)
+        txtR+="</tr>\n"
+print(txtR)
 
 #for data in filteredNews:
 #    print(data)
