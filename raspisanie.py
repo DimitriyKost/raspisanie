ab303229783f3272188c20edfdc65b449755bf67
DimitriyKost <dimkos08@gmail.com>
2021-07-04 16:13:15 +0300

    Исправления в форматировании талицы

--- raspisanie.py
+++ raspisanie.py
@@ -12,20 +12,25 @@ with open (file_htm,'r') as f:
 #allNews = []
 #print(html)
 pars1 = html.findAll('tr')
-txtH="   <td style='text-align: center;'><span style='font-size: 14pt;'>"
+txtH="            <td style='text-align: center;'><span style='font-size: 14pt;'>"
 #print (txtH)
 txtE="</td>\n"
-txtR="<table border='1'>\n<tbody>"
+txtR="<table border='1'>\n    <tbody>\n        <tr>\n\
+            <td style='text-align: center;' colspan='2' align='center' width='10%'><span style='font-size: 14pt;'>Дата</span></td>\n\
+            <td style='text-align: center;' width='50%'><span style='font-size: 14pt;'>Память святого или события</span></td>\n\
+            <td style='text-align: center;' width='20%'><span style='font-size: 14pt;'>Утро</span></td>\n\
+            <td style='text-align: center;' width='20%'><span style='font-size: 14pt;'>Вечер</span></td>\n\
+        </tr>\n"
 for i in range (len(pars1)):
         pars2 = pars1[i].findAll('td')
-        txtR+="<tr>\n"
+        txtR+="        <tr>\n"
         for j in range (len(pars2)):
             pars3 = pars2[j].find('span').text
             pars3=pars3.replace('\n ',' ')
             pars3=pars3.replace('  ',' ')
             txtR+=txtH+pars3+txtE
-        txtR+="</tr>\n"
-txtR+="</tbody>\n</table>"
+        txtR+="        </tr>\n"
+txtR+="    </tbody>\n</table>"
 print(txtR)
 
 #for data in filteredNews:
